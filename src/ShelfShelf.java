ae72d5bae8a5ea4f5adeaaf2712674687dce4c58
donal <25458345@studentmail.ul.ie>
2026-03-09 10:57:33 +0000

    shelf layering

---
+++ src/ShelfShelf.java
@@ -0,0 +1,19 @@
+public class ShelfShelf
+{
+    Shelf[] shelves;
+
+    public ShelfShelf()
+    {
+        this.shelves = null;
+    }
+
+    public void add(Shelf shelf) {
+        Shelf[] tempShelves = new Shelf[shelves.length+1];
+
+        for (int i = 0; i < tempShelves.length; i++) {
+            tempShelves[i] = shelves[i];
+        }
+        tempShelves[shelves.length] = shelf;
+        shelves = tempShelves;
+    }
+}
