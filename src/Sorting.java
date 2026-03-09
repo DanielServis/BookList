ee1f38d6a2cdf4d192dbf43ae339fae4cd807e1a
Artur SiudaMArtin <artursiuda20@gmail.com>
2026-03-09 10:56:43 +0000

    Removed Removing, Added basic sort

---
+++ src/Sorting.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Sorting
+{
+    public static ArrayList<Book> sorting(ArrayList<Book> arrayL)
+    {
+        ArrayList<Book> sortedAL = new ArrayList<Book>();
+
+
+        return sortedAL;
+    }
+}
