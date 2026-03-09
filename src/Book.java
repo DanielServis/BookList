ee010f262576d0735fb84cd5d9631024e9882d4f
donal <25458345@studentmail.ul.ie>
2026-03-09 10:20:42 +0000

    Book get/set functionality

--- src/Book.java
+++ src/Book.java
@@ -18,4 +18,69 @@ public class Book
         this.releaseYear = releaseYear;
         this.isbn = isbn;
     }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public String getPublisher(){
+        return publisher;
+    }
+
+    public int getReleaseYear(){
+        return releaseYear;
+    }
+
+    public int getIsbn(){
+        return isbn;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+
+    public void setLanguage(String language)
+    {
+        this.language = language;
+    }
+
+    public void setGenre(String genre)
+    {
+        this.genre = genre;
+    }
+
+    public void setPublisher(String publisher)
+    {
+        this.publisher = publisher;
+    }
+
+    public void setReleaseYear(int releaseYear)
+    {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setIsbn(int isbn)
+    {
+        this.isbn = isbn;
+    }
 }
