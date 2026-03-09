5ad14746eff284eb498a80f7b70c9188d8defa3e
donal <25458345@studentmail.ul.ie>
2026-03-09 11:10:32 +0000

    shelf super access

--- src/Shelf.java
+++ src/Shelf.java
@@ -1,13 +1,18 @@
 public class Shelf
 {
-    Book[] books;
+    static int IDC = 0;
+
+    private Book[] books;
+    private int ID;
 
     public Shelf()
     {
         this.books = null;
+        this.ID = IDC++;
     }
 
-    public void add(String title, String author, String language, String genre, String publisher, int releaseYear, int isbn) {
+    public void add(String title, String author, String language, String genre, String publisher, int releaseYear, int isbn)
+    {
         Book newBook = new Book(title, author, language, genre, publisher, releaseYear, isbn);
         Book[] tempBooks = new Book[books.length+1];
 
@@ -16,6 +21,11 @@ public class Shelf
         }
         tempBooks[books.length] = newBook;
         books = tempBooks;
+    }
+
+    public Book[] superGet()
+    {
+        return books;
     }
 
     public void remove(int number)
